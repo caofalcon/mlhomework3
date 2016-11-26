fe3e7fce35c9f125c2f031dbf4c49ea918fa75be
RJ <wzr@live.dk>
2016-11-26 13:00:13 -0500

    load labels and images

--- util.py
+++ util.py
@@ -15,14 +15,25 @@ class image :
 
 def LoadData(fname):
     """ Loads data """
-    img_training = [imread('train\\' + image_path) for image_path in os.listdir('train')]
+    img_training = [imread('train/' + image_path) for image_path in os.listdir('train')]
     images_training_obj = []
     for i in range(1, len(img_training) + 1):
         images_training_obj.append(image(img_training[i - 1], i))
 
-    print(np.genfromtxt('train.csv'))
+    with open('train.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        labs = {}
+        for row in spamreader:
+            try:
+                int(row[1])
+            except:
+                continue
+            labs[row[0]] = int(row[1])
 
+    for obj in images_training_obj:
+        obj.add_label(labs[str(obj.id)])
 
+    print(images_training_obj[0:10])
     return images_training_obj
 
 
