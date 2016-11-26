4a17c0e9ccfc1fc5e3891e4215ec399829d779a3
RJ <wzr@live.dk>
2016-11-26 13:47:51 -0500

    splitted images into test and train

--- util.py
+++ util.py
@@ -1,7 +1,9 @@
-import numpy as np
 from skimage.io import imread
 import os
 import csv
+import random
+
+test_images_ratio = 0.25
 
 class image :
 
@@ -16,9 +18,9 @@ class image :
 def LoadData(fname):
     """ Loads data """
     img_training = [imread('train/' + image_path) for image_path in os.listdir('train')]
-    images_training_obj = []
+    training_images = []
     for i in range(1, len(img_training) + 1):
-        images_training_obj.append(image(img_training[i - 1], i))
+        training_images.append(image(img_training[i - 1], i))
 
     with open('train.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -30,12 +32,18 @@ def LoadData(fname):
                 continue
             labs[row[0]] = int(row[1])
 
-    for obj in images_training_obj:
+    for obj in training_images:
         obj.add_label(labs[str(obj.id)])
 
-    print(images_training_obj[0:10])
-    return images_training_obj
+    random.shuffle(training_images)
 
+    training_images, test_images = training_images[0: int(len(training_images) * (1 - test_images_ratio))], \
+                                   training_images[int(len(training_images) * (1 - test_images_ratio)):]
+    print('done loading')
+    return training_images, test_images
+
+#TODO: pad
+#TODO: check if need contrast adjustments
 
 if __name__ == '__main__':
     a = LoadData('train.tar.gz')
